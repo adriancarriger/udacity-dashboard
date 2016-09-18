4d35ef3c1e459f058553f2ccd7b67ed6b2eeeffc
Adrian Carriger <mail@adrianandnicole.com>
2016-09-18 09:56:41 -0700

    feat(map): dynamic height, hide until loaded

--- src/app/geospacial/geospacial.component.ts
+++ src/app/geospacial/geospacial.component.ts
@@ -8,16 +8,30 @@ import { MapStyles } from './map-styles';
   encapsulation: ViewEncapsulation.None
 })
 export class GeospacialComponent {
+  @ViewChild('map') map;
   @ViewChild('branchContainer') branchContainer;
   public isCollapsed: boolean = true;
-
-  public zoom: number = 6;
-  public lat: number = 41.4090;
-  public lng: number = -75.6624;
-  public styles = MapStyles;
+  public mapMeta = {
+    current: {
+      lat: 41.4090,
+      lng: -75.6624
+    },
+    zoom: 6,
+    height: '500px',
+    styles: MapStyles,
+    loaded: false
+  }
+  public testingBranch = { // Scranton
+    lat: 41.4090,
+    lng: -75.6624
+  }
 
 
   constructor() { }
+
+  ngOnInit() {
+    this.mapMeta.height = (window.innerHeight - this.map.nativeElement.offsetTop) + 'px';
+  }
 
   onIdle() {
     this.branchContainer.nativeElement.parentElement.parentElement.parentElement.previousSibling.classList.add("map-label");
