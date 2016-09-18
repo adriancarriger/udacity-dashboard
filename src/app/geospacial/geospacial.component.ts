4612d8d858b2a22fa4e0f142545042a31e6ac021
Adrian Carriger <mail@adrianandnicole.com>
2016-09-17 22:31:55 -0700

    feat: add geospacial map

--- src/app/geospacial/geospacial.component.ts
+++ src/app/geospacial/geospacial.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ViewEncapsulation } from '@angular/core';
+import { MapStyles } from './map-styles';
 
 @Component({
   selector: 'app-geospacial',
   templateUrl: 'geospacial.component.html',
-  styleUrls: ['geospacial.component.scss']
+  styleUrls: ['geospacial.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GeospacialComponent {
+  @ViewChild('branchContainer') branchContainer;
   public isCollapsed: boolean = true;
-  
+
+  public zoom: number = 6;
+  public lat: number = 41.4090;
+  public lng: number = -75.6624;
+  public styles = MapStyles;
+
+
   constructor() { }
+
+  onIdle() {
+    this.branchContainer.nativeElement.parentElement.parentElement.parentElement.previousSibling.classList.add("map-label");
+  }
+
 }
--- src/app/geospacial/geospacial.module.ts
+++ src/app/geospacial/geospacial.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AgmCoreModule } from 'angular2-google-maps/core';
 
 import { SharedModule } from '../shared/shared.module';
 import { GeospacialComponent } from './geospacial.component';
 
 @NgModule({
-  imports: [CommonModule, SharedModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    AgmCoreModule.forRoot({
+        apiKey: 'AIzaSyBE1Bb86PEGx-11LahjWCZS2cFOWMpNseI'
+    })
+  ],
   declarations: [
     GeospacialComponent
   ],
