68b351e00bb2eeeaa2c60eaa886d7bec3ee37a47
Adrian Carriger <mail@adrianandnicole.com>
2016-12-04 09:48:46 -0800

    fix(geospacial): remove map update flicker

--- src/app/geospacial/geospacial.component.ts
+++ src/app/geospacial/geospacial.component.ts
@@ -3,9 +3,7 @@ import {
   ViewChild,
   ViewChildren,
   ViewEncapsulation,
-  OnInit,
-  AfterViewInit,
-  OnDestroy
+  OnInit
 } from '@angular/core';
 
 import { mapStyles } from './map-styles';
@@ -17,7 +15,7 @@ import { ApiService } from '../shared/api/api.service';
   styleUrls: ['geospacial.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class GeospacialComponent implements OnInit, AfterViewInit, OnDestroy {
+export class GeospacialComponent implements OnInit {
   @ViewChild('map') map;
   @ViewChildren('branches') branches;
   public isCollapsed: boolean = true;
@@ -25,7 +23,7 @@ export class GeospacialComponent implements OnInit, AfterViewInit, OnDestroy {
   public mapMeta = {
     lat: 41.4090,
     lng: -75.6624,
-    zoom: 6,
+    zoom: 7,
     height: undefined,
     styles: mapStyles,
     loaded: false
@@ -37,45 +35,4 @@ export class GeospacialComponent implements OnInit, AfterViewInit, OnDestroy {
   public ngOnInit(): void {
     this.mapMeta.height = (window.innerHeight - this.map.nativeElement.offsetTop) + 'px';
   }
-
-  ngAfterViewInit(): void {
-    this.subscription = this.branches.changes.subscribe(children => {
-      this.updateLabels();
-    });
-  }
-
-  public ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-  public updateLabels(): void {
-    setTimeout( () => this.addClassToLabels(), 0);
-  }
-
-  public scheduleUpdate(): void {
-    setTimeout( () => {
-      this.addClassToLabels();
-    }, 500);
-  }
-
-  private addClassToLabels(): void {
-    let scheduleUpdate = false;
-    this.isOpen = true;
-    this.branches.forEach(branch => {
-      if (branch.content === null || branch.content.parentElement === null) {
-        return;
-      }
-      let el = branch.content.parentElement.parentElement.previousSibling;
-      if (el !== undefined && el.classList !== undefined) {
-        el.classList.add('map-label');
-      }
-      if (el === undefined || el.classList === undefined || !el.classList.contains('map-label')) {
-        scheduleUpdate = true;
-      }
-    });
-   if (scheduleUpdate) {
-     this.scheduleUpdate();
-   }
-  }
-
 }
