8bade205ab200c5db30b1e5f4466413242d1a8b7
Adrian Carriger <mail@adrianandnicole.com>
2016-12-05 09:45:55 -0800

    feat(api): simplify api update pattern

--- src/app/shared/api/api.service.ts
+++ src/app/shared/api/api.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
 import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 import * as moment from 'moment';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ApiService {
@@ -14,7 +14,6 @@ export class ApiService {
   public lastUpdate: FirebaseObjectObservable<any>;
   public updateRunning: FirebaseObjectObservable<any>;
   public routeInfo: FirebaseObjectObservable<any>;
-  public interval;
   constructor(af: AngularFire, private http: Http) {
     this.branches = af.database.list('client/branches');
     this.sales = af.database.object('client/sales');
@@ -25,40 +24,42 @@ export class ApiService {
     this.updateRunning = af.database.object('server/run_info/running');
     this.routeInfo = af.database.object('client/tabs');
   }
-  public checkForUpdates(): void {
-    // If not running
-    this.updateRunning.subscribe( data => {
-      if (data.$value === false ) {
-        this.startRequests();
-      } else {
-        clearInterval( this.interval );
-      }
-    });
-    // If too much time since last update (on app init only)
-    this.lastUpdate.subscribe( data => {
-      let secondsSinceUpdate = (moment().valueOf() - data.$value) / 1000;
-      if (secondsSinceUpdate > 40) {
-        this.startRequests();
-      } else {
-        clearInterval( this.interval );
-      }
-    });
-  }
-
-  /** 
-   * Continue to request that the server start pushing updates
-   * until the first request comes in
+  /**
+   * **How this api works:**
+   * 
+   * 1. An AWS lambda will issue updates about every 5-10 seconds for up to 2-8 minutes to a
+   * Firebase database.
+   * 2. Firebase will then push updates to each client (usually through a websocket connection).
+   * 3. Upon the lambda update cycle completion, the api running status will change to false.
+   *   a. If no clients are listening, the lambda will do nothing further to conserve resources.
+   *   b. If any client is listening, it will detect the api running status change and send a get
+   * request to another lambda via {@link requestUpdates} and the server will start another cycle
+   * of updates.
    */
-  private startRequests(): void {
-    clearInterval( this.interval );
-    this.requestUpdates();
-    this.interval = setInterval( () => {
-      this.requestUpdates();
-    }, 8000);
+  checkForUpdates() {
+    this.updateRunning.subscribe(status => this.onApiStatusChange(status.$value));
+    this.lastUpdate.subscribe(lastUpdate => this.onLastUpdate(lastUpdate.$value));
   }
-
-  private requestUpdates(): void {
-    let url = 'https://ae373q5sa4.execute-api.us-west-2.amazonaws.com/prod/lambda';
-    this.http.get( url ).subscribe();
+  /**
+   * Triggered on each api running status change.
+   * 
+   * Will request further updates if the value is false.
+   */
+  private onApiStatusChange(status: boolean) {
+    if (!status) { this.requestUpdates(); }
+  }
+  /**
+   * If it has been more than 10 seconds since the last update, then request further updates.
+   */
+  private onLastUpdate(lastUpdate) {
+    let secondsSinceUpdate = (moment().valueOf() - lastUpdate) / 1000;
+    if (secondsSinceUpdate > 10) { this.requestUpdates(); }
+  }
+  /**
+   * Requests that the AWS lambda start pushing updates.
+   */
+  private requestUpdates() {
+    let apiUrl = 'https://ae373q5sa4.execute-api.us-west-2.amazonaws.com/prod/lambda';
+    this.http.get(apiUrl).subscribe();
   }
 }
